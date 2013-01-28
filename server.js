7309162f46bc406042edd25853dc332eeeae6832
knowowl <ceo@know-owl.com>
2013-01-28 14:45:34 -0430

    RequestHandlers

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var http = require("http");
 var url = require("url");
 var port = process.env.PORT || 5000;
-function iniciar(route) {
+
+function iniciar(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Peticion para " + pathname + " recibida.");
 
-    route(pathname);
-
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Hola Mundo");
+    var content = route(handle, pathname)
+    response.write(content);
     response.end();
   }
 
