77894e27576cc19c1b9c8e27bc31e284ac8b0288
knowowl <ceo@know-owl.com>
2013-01-28 20:41:12 -0430

    test stylus 3

--- app.js
+++ app.js
@@ -4,12 +4,23 @@
  */
 
 var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
+  
+
+
 var app = express();
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -17,6 +28,11 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
+  app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
