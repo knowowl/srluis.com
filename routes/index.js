0570ee904cc9d92141737895499f52f115839653
knowowl <ceo@know-owl.com>
2013-02-01 20:55:55 -0430

    json

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ exports.contact = function(req, res){
 
 exports.search = function(req, res) {
 	res.contentType('application/json');
-	db.store.find({},function(err, user) {
+	store.find(function(err, user) {
 	  if (err) {console.log(err);}
       if (user != null) {
         console.log('Found the User:' + user.DisplayName);
