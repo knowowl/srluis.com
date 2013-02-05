d4787b4545eac1b397b0577bdd7681126dd91f4e
knowowl <ceo@know-owl.com>
2013-02-05 15:49:28 -0430

    loadImage.js

--- public/js/fb.js
+++ public/js/fb.js
@@ -1,17 +1,5 @@
 
-window.fbAsyncInit = function() {
-    // init the FB JS SDK
-    FB.init({
-      appId      : '390325411063913', // App ID from the App Dashboard
-     // channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html',  Channel File for x-domain communication
-      status     : true, // check the login status upon init?
-      cookie     : true, // set sessions cookies to allow your server to access the session?
-      xfbml      : true  // parse XFBML tags on this page?
-    });
 
-    // Additional initialization code such as adding Event Listeners goes here
-
-  };
 
   // Load the SDK's source Asynchronously
   // Note that the debug version is being actively developed and might 
