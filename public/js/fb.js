16f41e7befddd8ee5c4c37d25f5d1098b325648b
knowowl <ceo@know-owl.com>
2013-02-05 15:18:25 -0430

    loadImage.js

--- public/js/fb.js
+++ public/js/fb.js
@@ -1,6 +1,9 @@
 
 
-
+window.fbAsyncInit = function() {
+				FB.init({status: true});
+			
+			};
    (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
