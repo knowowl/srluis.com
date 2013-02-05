a54017c2876024e84447723e61e8b3bf7b13834f
knowowl <ceo@know-owl.com>
2013-02-05 15:32:43 -0430

    loadImage.js

--- public/js/fb.js
+++ public/js/fb.js
@@ -1,9 +1,18 @@
 
 
 window.fbAsyncInit = function() {
-				FB.init({status: true});
-			
-			};
+    // init the FB JS SDK
+    FB.init({
+      appId      : '390325411063913', // App ID from the App Dashboard
+       // Channel File for x-domain communication
+      status     : true, // check the login status upon init?
+      cookie     : true, // set sessions cookies to allow your server to access the session?
+      xfbml      : true  // parse XFBML tags on this page?
+    });
+
+    // Additional initialization code such as adding Event Listeners goes here
+
+  };
    (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
