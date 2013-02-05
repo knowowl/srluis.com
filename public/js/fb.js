19be0ae4fbac5faf78b1a21d6926b92b98b5157f
knowowl <ceo@know-owl.com>
2013-02-05 15:47:32 -0430

    loadImage.js

--- public/js/fb.js
+++ public/js/fb.js
@@ -1,9 +1,9 @@
 
-
 window.fbAsyncInit = function() {
     // init the FB JS SDK
     FB.init({
-      appId      : '390325411063913', // App ID from the App Dashboar
+      appId      : '390325411063913', // App ID from the App Dashboard
+     // channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html',  Channel File for x-domain communication
       status     : true, // check the login status upon init?
       cookie     : true, // set sessions cookies to allow your server to access the session?
       xfbml      : true  // parse XFBML tags on this page?
@@ -17,10 +17,10 @@ window.fbAsyncInit = function() {
   // Note that the debug version is being actively developed and might 
   // contain some type checks that are overly strict. 
   // Please report such bugs using the bugs tool.
-  (function(d, debug){
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "http://connect.facebook.net/es_ES/all" + (debug ? "/debug" : "") + ".js";
-     ref.parentNode.insertBefore(js, ref);
-   }(document, /*debug*/ false));
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "http://connect.facebook.net/es_ES/all.js#xfbml=1&appId=390325411063913";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
