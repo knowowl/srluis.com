4c8381973ea8c2fc3ece66da44587481f90dfbbf
knowowl <ceo@know-owl.com>
2013-02-09 17:07:44 -0430

    order db

--- app.js
+++ app.js
@@ -79,13 +79,10 @@ app.get('/search', loadSearch(), function(req, res, next) {
 
 app.get('/order', function(req, res) {
     res.contentType('application/json');      
-    order.findOne({}, function(err, user) {
-      if (user != null) {
-        console.log('Found the User:' + user.username);
+    order.findOne({'user_id': 'test', 'state':'cart'}, function(err, user) {
+     
         res.json(user);
-      }else{
-         console.log('error:' + err);
-      }
+    
     });
 });
 
